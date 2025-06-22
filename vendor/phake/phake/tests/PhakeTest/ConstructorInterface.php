56a9110fc652efe1ac0aaa5036635873f568f900
Noah Smith <inoahsmi@amazon.com>
2025-06-22 15:06:15 -0500

    .gitignore

--- vendor/phake/phake/tests/PhakeTest/ConstructorInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-interface PhakeTest_ConstructorInterface
-{
-    public function __construct(PhakeTest_A $a);
-}
--- vendor/phake/phake/tests/PhakeTest/PDOExtendingClass.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-class PhakeTest_PDOExtendingClass extends PDO
-{
-}
