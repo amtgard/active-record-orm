1622dfb2537a084cb1173283a8eb93718e974cc3
Noah Smith <inoahsmi@amazon.com>
2025-12-12 08:23:07 -0600

    Fixes an issue where traits hiding defined EntityRepository fields causes field values to not be exposed after fetch or written after set

--- src/Interface/EntityInterface.php
+++ src/Interface/EntityInterface.php
@@ -4,6 +4,7 @@ namespace Amtgard\ActiveRecordOrm\Interface;
 
 use Amtgard\ActiveRecordOrm\Entity\EntityMapper;
 use Amtgard\ActiveRecordOrm\Schema\FieldDefinition;
+use Amtgard\ActiveRecordOrm\Schema\TableSchema;
 
 interface EntityInterface
 {
@@ -14,5 +15,5 @@ interface EntityInterface
 
     function persist(EntityMapper $mapper);
 
-
+    function getSchema(): TableSchema;
 }
---
+++ tests/integ/AuditRepositoryEntityTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Tests\Integration;
+
+class AudientRepositoryEntityTest
+{
+
+}
